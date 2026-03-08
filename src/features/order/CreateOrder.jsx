403498a8514cfde1862546006c780b252f4756c0
Natnael Endale <n87294578@gmail.com>
2026-03-08 13:11:05 -0700

    add error alert to phone number input

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -3,35 +3,12 @@ import { useSelector } from "react-redux";
 import { createOrder } from "../../services/apiRestaurant";
 import Button from "../../ui/Button";
 import { getCart } from "../cart/cartSlice";
+import EmptyCart from "../cart/EmptyCart";
 
 const isValidPhone = (str) =>
   /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
     str,
   );
-
-const fakeCart = [
-  {
-    pizzaId: 12,
-    name: "Mediterranean",
-    quantity: 2,
-    unitPrice: 16,
-    totalPrice: 32,
-  },
-  {
-    pizzaId: 6,
-    name: "Vegetale",
-    quantity: 1,
-    unitPrice: 13,
-    totalPrice: 13,
-  },
-  {
-    pizzaId: 11,
-    name: "Spinach and Mushroom",
-    quantity: 1,
-    unitPrice: 15,
-    totalPrice: 15,
-  },
-];
 
 function CreateOrder() {
   const navigation = useNavigation();
@@ -40,6 +17,10 @@ function CreateOrder() {
   const formErrors = useActionData();
   const username = useSelector((state) => state.user.name);
   const cart = useSelector(getCart);
+
+  if (cart.length === 0) {
+    return <EmptyCart />;
+  }
 
   return (
     <div className="px-4 py-6">
