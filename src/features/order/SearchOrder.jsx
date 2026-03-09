87a659275f00186ede018637b996b7eea86ee848
Natnael Endale <n87294578@gmail.com>
2026-03-09 06:45:00 -0700

    add the style to serach btn

--- src/features/order/SearchOrder.jsx
+++ src/features/order/SearchOrder.jsx
@@ -18,7 +18,7 @@ function SearchOrder() {
         placeholder="Search order #"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
-        className="focus:ring-opacity-50 w-28 rounded-full bg-yellow-100 px-4 py-2 text-sm transition-all duration-300 placeholder:text-stone-400 focus:ring focus:ring-yellow-500 focus:outline-none sm:w-64 sm:focus:w-92"
+        className="input w-36 text-sm sm:w-52 sm:text-base md:w-60 md:focus:w-72"
       />
     </form>
   );
