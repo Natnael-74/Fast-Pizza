5cf4051421cbe6f688a7e9b02cad1dadc4d5e5ac
Natnael Endale <n87294578@gmail.com>
2026-03-09 02:30:55 -0700

    add the style

--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -3,7 +3,7 @@ import CreateUser from "../features/user/CreateUser";
 import Button from "./Button";
 
 function Home() {
-  const username = useSelector((state) => state.user.name);
+  const username = useSelector((state) => state.user.username);
 
   return (
     <div className="my-10 px-4 text-center sm:my-16">
